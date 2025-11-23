fe7ad620bae4fa78ee2d3449ec8352c0de31c6d6
Tushar Bhore <tusharvbhore@gmail.com>
2025-11-24 00:04:27 +0530

    Add files via upload

---
+++ Assignments/Assignment_30/Program_1.c
@@ -0,0 +1,57 @@
+#include<stdio.h>
+void DisplayPattern(int iRow,int iCol)
+{
+    int i=0,j=0;
+
+    if(iRow != iCol)
+    {
+
+        printf("Invalid input. \n");
+        printf("Enter the same number of rows and column.\n");
+
+    }
+    else
+    {
+    
+        for(i=1; i <= iRow; i++)
+        {
+            for(j=1; j <= iCol; j++)
+            {
+                if(i==j || i>j)
+                {
+
+                    printf(" * \t");
+
+                }
+                else
+                {
+
+                    printf(" # \t");
+              
+                }
+           
+            }
+       
+            printf("\n");
+
+    }
+}
+
+
+}
+
+int main()
+{
+    int iValue1=0,iValue2;
+
+    printf("Enter the number of rows : \t");
+    scanf("%d",&iValue1);
+
+    printf("Enter the number of colums : \t");
+    scanf("%d",&iValue2);
+
+    DisplayPattern(iValue1 , iValue2);
+
+
+    return 0;
+}
